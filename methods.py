96a82f0509a8063b539227088fbb5871889c7ca0
ankitgujre <ankitgujre374@gmail.com>
2026-02-05 11:22:21 +0530

    list methods, and slicing

---
+++ methods.py
@@ -0,0 +1,22 @@
+# lst = [2,1,3]
+lst = ["banana", "litchi", "apple"]
+
+lst.append("4")  # add element at the end of list
+print(lst)
+
+lst.sort()
+print(lst)  # sort in ascending order by default
+
+lst.sort(reverse=True)
+print(lst)
+
+lst.reverse()
+print(lst)
+
+lst.insert(1, 5)
+print(lst)
+
+
+# lst = [2,1,3]
+# lst.append(4)
+# print(lst)
---
+++ slicing.py
@@ -0,0 +1,7 @@
+marks = [85,65,55.3,68,95]
+print(marks[1])
+print(marks[1:])
+print(marks[:])
+print(marks[:-1])
+print(marks[-3:-1])
+print(marks[1:len(marks)])
